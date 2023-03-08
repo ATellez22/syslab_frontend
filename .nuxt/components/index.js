81a13fed415e41301dba8e013e34382f7bb2c386
ATellez22 <atellez@juarismi.com>
2023-03-08 19:36:50 -0300

    Base Template Adjustment

---
+++ .nuxt/components/index.js
@@ -0,0 +1,35 @@
+export { default as AdminTemplate } from '../../components/AdminTemplate.vue'
+export { default as BaseAside } from '../../components/base/Aside.vue'
+export { default as BaseFooter } from '../../components/base/Footer.vue'
+export { default as BaseNavbar } from '../../components/base/Navbar.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
